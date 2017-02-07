d7c635569205977acb0c53015dc10c6904c9406b
ANURAGTIWARI\Anurag <anuragti@usc.edu>
2017-02-07 13:42:24 -0800

    Version 1
    First commit

---
+++ Assets/scripts/PlayerVitals.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerVitals : MonoBehaviour
+{
+    public Slider healthSlider;
+    public int maxHealth;
+    public int healthFallRate;
+
+    public Slider staminaSlider;
+    public int maxStamina;
+    private int staminaFallRate;
+    public int staminaFallMult;
+    private int staminaRegainRate;
+    public int staminaRegainMult;
+
+    public Slider hungerSlider;
+    public int maxHunger;
+    public int hungerFallRate;
+
+
+    public Slider thristSlider;
+    public int maxThrist;
+    public int thirstFallRate;
+
+    private CharacterController charController;
+    private FirstPersonController playerController;
+
+    void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+
+        thristSlider.maxValue = maxThrist;
+        thristSlider.value = maxThrist;
+
+        hungerSlider.maxValue = maxHunger;
+        hungerSlider.value = maxHunger;
+
+        staminaSlider.maxValue = maxStamina;
+        staminaSlider.value = maxStamina;
+        staminaFallRate = 1;
+        staminaRegainRate = 1;
+
+        charController = GetComponent<CharacterController>();
+        playerController = GetComponent<FirstPersonController>();
+    }
+    void Update()
+    {
+        //health degrade
+        if(hungerSlider.value<=0 && thristSlider.value<=0)
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate * 2;
+        }
+        else if(hungerSlider.value <=0 || thristSlider.value<=0)
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate * 2;
+
+        }
+        if(healthSlider.value<=0)
+        {
+            //player death. DO SOMETHING
+        }
+
+        //hungerController
+        if(hungerSlider.value>=0)
+        {
+            hungerSlider.value -= Time.deltaTime / hungerFallRate;
+        }
+
+        //thirstController
+        if (thristSlider.value >= 0)
+        {
+            thristSlider.value -= Time.deltaTime / thirstFallRate;
+        }
+        //thrist dependency on stamina
+        if(staminaSlider.value <=0)
+        {
+            thristSlider.value -= Time.deltaTime * 2 / thirstFallRate;
+        }
+        else if (staminaSlider.value >=0)
+        {
+            thristSlider.value -= Time.deltaTime / thirstFallRate ;
+        }
+
+        //this controlls the stamina system
+        if (charController.velocity.magnitude > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            staminaSlider.value -= Time.deltaTime / staminaFallRate * staminaFallMult;
+        }
+        else
+        {
+            staminaSlider.value += Time.deltaTime / staminaRegainRate * staminaRegainMult;
+        }
+        if(staminaSlider.value >= maxStamina)
+        {
+            staminaSlider.value = maxStamina;
+        }
+        else if (staminaSlider.value <= 0)
+        {
+            staminaSlider.value = 0;
+            playerController.m_RunSpeed = playerController.m_WalkSpeed;
+        }
+        else if (staminaSlider.value >= 0)
+        {
+            playerController.m_RunSpeed = playerController.m_RunSpeedNorm;
+        }
+    }
+
+}
---
+++ Assets/scripts/chase.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chase : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform head;
+    Animator anim;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float rotSpeed = 0.2f;
+    public float speed = 1.5f;
+    float accuracyWP = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - this.transform.position; //get the vector between player and enemy
+       // direction.y = 0; //to prevent vertical rotation-> tipping over
+        float angle = Vector3.Angle(direction, head.up);
+
+        if (state == "patrol" && waypoints.Length > 0)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalk", true);
+            if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+            {
+
+               // currentWP = Random.Range(0, waypoints.Length); // this is to randomize the current position waypoint
+                currentWP++;
+                if(currentWP >= waypoints.Length)
+                {
+                	currentWP = 0;
+                }	
+            }
+
+            //rotate towards waypoint
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation,
+                                 Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 60 || state == "pursuing"))
+        {
+
+            state = "pursuing";
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                        Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+
+            if (direction.magnitude > 3)
+            {
+                this.transform.Translate(0, -0.005f, Time.deltaTime * speed);
+                anim.SetBool("isWalk", true);
+                anim.SetBool("isAttack", false);
+            }
+            else
+            {
+                anim.SetBool("isWalk", false);
+                anim.SetBool("isAttack", true);
+                
+            }
+
+        }
+        else
+        {
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isAttack", false);
+            state = "patrol";
+        }
+
+    }
+}
